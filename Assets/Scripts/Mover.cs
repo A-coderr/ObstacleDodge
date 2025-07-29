ada7d53e1d83b6675dc9a96512cb18e6077f6d33
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2025-07-29 11:51:19 -0400

    Serialized the fields

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float xValue = 0.01f;
+    [SerializeField] float yValue = 0f;
+    [SerializeField] float zValue = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
     }
 }
