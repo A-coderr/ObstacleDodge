22744ea4b8286c0f82ffc4d50603125f15b9be3e
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2025-11-03 14:30:12 -0500

    Added a flying bullet

---
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class FlyAtPlayer : MonoBehaviour
+{
+    [SerializeField] float speed = 0.01f;
+    [SerializeField] Transform player;
+    Vector3 playerPosition;
+    void Start()
+    {
+        playerPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, Time.deltaTime * speed);
+    }
+}
